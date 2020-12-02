c3b6d770990211cf554175f73e362b665cae1b1d
Mehedi Hasan <mehedihasansony1@hotmail.com>
2020-12-02 18:52:28 +0800

    camera init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Camera } from '@ionic-native/camera';
+
+import { DocumentScanner } from '@ionic-native/document-scanner/ngx';
+import { FileOpener } from '@ionic-native/file-opener';
+import { File } from '@ionic-native/file';
+import { ImagePicker } from '@ionic-native/image-picker';
+import { ImageResizer } from '@ionic-native/image-resizer';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
+import { Base64 } from '@ionic-native/base64';
+import { CameraPreview} from '@ionic-native/camera-preview';
+import { Crop } from '@ionic-native/crop';
 
 @NgModule({
   declarations: [
@@ -14,7 +25,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +35,16 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    DocumentScanner,
+    File,
+    FileOpener,
+    ImagePicker,
+    ImageResizer,
+    Base64ToGallery,
+    Base64,
+    CameraPreview,
+    Crop,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,104 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import {
+  CameraPreview,
+  CameraPreviewOptions,
+} from "@ionic-native/camera-preview";
+// import { Crop } from "@ionic-native/crop";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  smallPreview: boolean;
+  IMAGE_PATH: any;
+  colorEffect = "none";
+  setZoom = 1;
+  flashMode = "off";
+  isToBack = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private cp: CameraPreview) {}
 
+  startCameraAbove() {
+    const cpOption: CameraPreviewOptions = {
+      x: 80,
+      y: 450,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: this.cp.CAMERA_DIRECTION.BACK,
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: false,
+      alpha: 1,
+    };
+    // this.cp.stopCamera().then((res) => {
+    //   console.log("startCameraAbove: " + res);
+      this.cp.startCamera(cpOption).then((res) => {
+        console.log("StartCamera: " + res);
+      });
+  //   });
+   }
+
+  startCameraBelow() {
+    const cpOption: CameraPreviewOptions = {
+      x: 0,
+      y: 50,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: this.cp.CAMERA_DIRECTION.FRONT,
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: true,
+      alpha: 1,
+    };
+
+    // this.cp.stopCamera().then((res) => {
+    //   console.log("startCameraBelow: " + res);
+      this.cp.startCamera(cpOption).then((res) => {
+        console.log("StartCamera: " + res);
+      });
+    // });
   }
 
+  takePicture() {
+    this.cp
+      .takePicture({
+        width: 1280,
+        height: 1280,
+        quality: 85,
+      })
+      .then(
+        (imageData) => {
+          this.IMAGE_PATH = "data:image/jpeg;base64," + imageData;
+        },
+        (err) => {
+          console.log(err);
+          this.IMAGE_PATH = "assets/img/test.jpg";
+        }
+      );
+  }
+
+  switchCamera() {
+    this.cp.switchCamera();
+  }
+
+  changeFlashMode() {
+    this.cp.setFlashMode(this.flashMode);
+  }
+
+  changeZoom() {
+    this.cp.setZoom(this.setZoom);
+  }
+
+  show() {
+    this.cp.show();
+  }
+  hide() {
+    this.cp.hide();
+  }
+
+  stopCamera() {
+    this.cp.stopCamera();
+  }
 }
